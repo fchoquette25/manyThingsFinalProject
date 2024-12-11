d203516a2b3e605c34a7f803512a9a002082d234
fchoquette25 <fchoquette25@avenues.org>
2024-12-10 19:13:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,7 @@ function setup() {
 
   for (let i = 0; i < 15; i++) {
     circles.push({
-      x: random(width),
-      y: random(height),
+     
       size: random(20, 50),
       sizeChange: random(0.5, 2), 
       color: color(random(255), random(255), random(255)),
